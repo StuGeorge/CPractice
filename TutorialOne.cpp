fa2c353604442097e360623bf99145e81272f6c4
Stu George <stugeorge1@gmail.com>
2014-11-05 21:27:59 +0000

    Hello world tutorials and whatnot

---
+++ TutorialOne.cpp
@@ -0,0 +1,18 @@
+//
+//  TutorialOne.cpp
+//  
+//
+//  Created by Stuart George on 05/11/2014.
+//
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello" << std::endl; // The std::endl prints an endline character (\n) and flushes the text buffer (no me neither...)
+    std::cout << "Hello to you too\n";
+    
+    return 0;
+    
+}
